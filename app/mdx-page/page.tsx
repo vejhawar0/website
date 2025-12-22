82d66538b8f3ded1aeaa7a644f36515ba44c1e18
Vedant Jhawar <vjhawar@Mac.localdomain>
2025-12-21 17:25:50 -0800

    Added basic css for darkening (needs to be updated/streamlined. Attempting to change structure of posts

--- app/mdx-page/page.tsx
+++
@@ -1,6 +0,0 @@
-import Welcome from "./welcome.mdx";
-
-
-export default function Page() {
-  return <Welcome />
-}
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,22 @@
 import type { MDXComponents } from 'mdx/types'
+import Link from "next/link";
  
-const components: MDXComponents = {}
+const components: MDXComponents = 
+{
+    a: (props) => <Link {...props} />,
+    
+    Warning: ({ children }) => (
+      <div style={{
+        background: "#222",
+        borderLeft: "4px solid #f39c12",
+        padding: "1rem",
+        borderRadius: "6px",
+        margin: "1rem 0"
+      }}>
+        ⚠️ {children}
+      </div>
+    ),
+}
  
 export function useMDXComponents(): MDXComponents {
   return components
