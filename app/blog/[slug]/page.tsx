411423abba8d2b68d41af165aae28e8d3ed9152f
Vedant Jhawar <vjhawar@Vedants-MacBook-Air.local>
2025-12-22 20:35:23 -0800

    Added dynamic routing for blog content

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,39 @@
+export const dynamic = "force-static"
+
+import { getAllPosts, getPostBySlug } from "@/lib/posts"
+import { notFound } from "next/navigation"
+import { MDXRemote } from "next-mdx-remote/rsc"
+
+type Props = {
+  params: {
+    slug: string
+  }
+}
+
+export async function generateStaticParams() {
+  const posts = getAllPosts()
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }))
+}
+
+export default async function BlogPostPage({ params, }: {params: Promise<{ slug: string }>}) {
+  
+    const { slug } = await(params) 
+    let post
+
+    try {
+        post = getPostBySlug(slug)
+    } catch {
+        notFound()
+    }
+    return (
+        <article className="prose mx-auto py-10">
+        <h1>{post.title}</h1>
+        <p className="text-sm text-gray-500">{post.date}</p>
+
+        <MDXRemote source={post.content} />
+        </article>
+    )
+}
---
+++ lib/posts.tsx
@@ -0,0 +1,51 @@
+import fs from "fs"
+import path from "path"
+import matter from "gray-matter"
+
+const BLOG_DIR = path.join(process.cwd(), "content/blog")
+// const PROJECTS_DIR = path.join(process.cwd(), "content/projects")
+
+export type Post = {
+  title: string
+  slug: string
+  date: string
+  description?: string
+  content: string
+}
+
+export function getAllPosts(): Post[] {
+  const files = fs.readdirSync(BLOG_DIR)
+
+  return files.map((file) => {
+    const filePath = path.join(BLOG_DIR, file)
+    const raw = fs.readFileSync(filePath, "utf-8")
+    const { data, content } = matter(raw)
+
+    // const { mtime } = fs.statSync();
+    // const lastModifiedDate = mtime.toISOString(); 
+
+    if (!data.slug) {
+      throw new Error(`Missing slug in ${file}`)
+    }
+
+    return {
+      title: data.title,
+      slug: data.slug,
+      date: String(data.date),
+      description: data.description,
+      content,
+    //   last_modified: lastModifiedDate
+    }
+  })
+}
+
+export function getPostBySlug(slug: string): Post {
+  const posts = getAllPosts()
+  const post = posts.find((p) => p.slug === slug)
+
+  if (!post) {
+    throw new Error(`Post not found for path: ${slug}`)
+  }
+
+  return post
+}
